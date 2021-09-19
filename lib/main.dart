2f56dcd97f821c29eec5dcf6404fcf466b3beaca
geohasby <geohasby@gmail.com>
2021-09-19 08:14:58 +0700

    add alert notif icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:badges/badges.dart';
 import 'dart:math';
 
 void main() {
@@ -63,8 +64,9 @@ class MyApp extends StatelessWidget {
   Color splash_color = Colors.blue;
 
   String unit = "";
-
   String value = "";
+  double val = 0;
+  bool alert = false;
 
   var rng = new Random();
 
@@ -137,68 +139,115 @@ class MyApp extends StatelessWidget {
                       children: List.generate(
                         variable.length,
                         (i) {
+                          alert = false;
                           if (i == 0) {
                             card_color = Colors.lightBlue;
                             splash_color = Colors.blue.shade800;
-                            value = (rng.nextInt(10) + 20).toInt().toString();
+                            val = rng.nextInt(10) + 20;
+                            if (val < 20 || val > 30) alert = true;
+                            value = val.toInt().toString();
                             unit = "°C";
                           } else if (i == 1) {
                             card_color = Colors.purple.shade400;
                             splash_color = Colors.deepPurple;
-                            value =
-                                (rng.nextInt(26) / 10 + 6.7).toStringAsFixed(1);
+                            val = rng.nextInt(26) / 10 + 6.7;
+                            if (val < 7.2 || val > 8.8) alert = true;
+                            value = val.toStringAsFixed(1);
                             unit = "";
                           } else if (i == 2) {
                             card_color = Colors.amber;
                             splash_color = Colors.deepOrange;
-                            value = (rng.nextInt(25) + 10).toInt().toString();
+                            val = rng.nextInt(25) + 10;
+                            if (val < 15 || val > 25) alert = true;
+                            value = val.toInt().toString();
                             unit = "‰";
                           } else if (i == 3) {
                             card_color = Colors.lightGreen;
                             splash_color = Colors.teal;
-                            value = (rng.nextInt(30) + 105).toInt().toString();
+                            val = rng.nextInt(30) + 105;
+                            if (val < 110 || val > 130) alert = true;
+                            value = val.toInt().toString();
                             unit = " mg/L";
                           }
-                          return Card(
-                            color: card_color,
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: InkWell(
-                              customBorder: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(16),
+                          return Stack(
+                            children: [
+                              Container(
+                                height: double.infinity,
+                                width: double.infinity,
+                                child: Card(
+                                  color: card_color,
+                                  elevation: 10,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                  child: InkWell(
+                                    customBorder: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    splashColor: splash_color,
+                                    onTap: () {},
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 10),
+                                          child: SvgPicture.asset(
+                                            'assets/' + variable[i] + '.svg',
+                                            height: 50,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                        Text(
+                                          value + unit,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 32,
+                                          ),
+                                        ),
+                                        Text(
+                                          variable[i],
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ),
-                              splashColor: splash_color,
-                              onTap: () {},
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(bottom: 10),
-                                    child: SvgPicture.asset(
-                                      'assets/' + variable[i] + '.svg',
-                                      height: 50,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  Text(
-                                    value + unit,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 32,
-                                    ),
-                                  ),
-                                  Text(
-                                    variable[i],
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                              alert == true
+                                  ? Positioned(
+                                      right: 0,
+                                      child: Container(
+                                        height: 30,
+                                        width: 30,
+                                        padding: EdgeInsets.all(2.0),
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(20.0),
+                                          color: Colors.red,
+                                        ),
+                                        constraints: BoxConstraints(
+                                          minHeight: 16,
+                                          minWidth: 16,
+                                        ),
+                                        child: Center(
+                                          child: Text(
+                                            '!',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  : Container()
+                            ],
                           );
                         },
                       ),
